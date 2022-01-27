e246850377dee0ef35fe9f79bc2576520a015d21
Shivammishra789 <shivammishra7789@gmail.com>
2022-01-27 23:33:34 +0530

    ADDED: api for cart

--- main.py
+++ main.py
@@ -7,9 +7,11 @@ from routes import user_api
 from routes import book_api
 from fastapi import FastAPI
 from routes import wishlist_api
+from routes import cart_api
 
 app = FastAPI(title="Book Store App")
 
 app.include_router(user_api.route)
 app.include_router(book_api.route)
 app.include_router(wishlist_api.route)
+app.include_router(cart_api.route)
